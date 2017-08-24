fc49222735d4dba0c27c75e9b99d67c4d629e8e3
JOY <JOY@192.168.0.6>
2017-08-24 18:12:59 +0530

    Contructor is added

--- sa/src/sa/Employee.java
+++ sa/src/sa/Employee.java
@@ -4,4 +4,16 @@ public class Employee {
 	
 	String name,email;
 
+	/**
+	 * @param name
+	 * @param email
+	 */
+	public Employee(String name, String email) {
+		
+		this.name = name;
+		this.email = email;
+	}
+	
+	
+
 }
