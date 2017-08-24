cdd60f92b8d95a4e1fd6ae5ff57d0b3f756ba804
KRISHNA <maddelakrishnakumar@gmail.com>
2017-08-24 23:39:38 +0530

    Home added

---
+++ onlineshoping/src/main/java/domain/User.java
@@ -0,0 +1,54 @@
+package domain;
+
+public class User {
+	private String uid,uname,email,mobile,password;
+
+	public String getUid() {
+		return uid;
+	}
+
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+
+	public String getUname() {
+		return uname;
+	}
+
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "User [uid=" + uid + ", uname=" + uname + ", email=" + email + ", mobile=" + mobile + "]";
+	}
+	
+	
+	
+
+}
